7c69c0ccae53696be84e639a43e4349bd03532e6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-31 17:58:49 -0800

    login added with error checking, logout added, next step error checking logout

--- server.py
+++ server.py
@@ -2,10 +2,13 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,14 +25,71 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/register")
+def register_form():
+
+    return render_template("register_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    try:
+        user_object = User.query.filter_by(email=username).one()
+        if password == user_object.password:
+            pass   # login -- for clairty in code
+        else:
+            flash("Wrong Password")
+            return redirect('/register')
+
+    except sqlalchemy.orm.exc.NoResultFound:
+        flash('user created: %s') % username
+        user_object = User(email=username,
+                           password=password)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user_object)
+
+        # Once we're done, we should commit our work
+        db.session.commit()
+
+    session['user_id'] = user_object.user_id
+    print(session)
+    flash("Logged In")
+    return redirect("/")
+
+
+@app.route('/logout')
+def logout_process():
+
+    del session['user_id']
+    flash('logged out')
+    return redirect('/register')
+
+
+
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = True
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     connect_to_db(app)
---
+++ static/scripts.js
@@ -0,0 +1,18 @@
+$('#registration').on('submit', function(evt) {
+    // evt.preventDefault();
+    var userChars = $('#username').val();
+    console.log('userChars: ' +userChars);
+    var passChars = $('#password').val().length;
+    if ((/(.+)@(.+\.){1,}(com|net|gov|edu)/g).test(userChars) === false) {
+        evt.preventDefault();
+        // flash("please enter a valid email");
+        alert('username not email');
+        // $.get('/register', "error: 'username not email'")
+    }
+    else if (passChars < 2) {
+        // flash('please enter a password');
+        alert('password not entered');
+        evt.preventDefault();
+    }
+
+});
